52d97fc25b404eb24ecda044f08b82cf103dbdd3
lsaudon <lsaudon@gmail.com>
2023-02-10 19:02:36 +0100

    feat: Find the unused translations if no class has identical properties

--- lib/src/commands/commands.dart
+++ lib/src/commands/commands.dart
@@ -1,3 +1,2 @@
 export 'check_unused_command.dart';
-export 'sample_command.dart';
 export 'update_command.dart';
--- lib/src/visitors/l10n_visitor.dart
+++ lib/src/visitors/l10n_visitor.dart
@@ -2,16 +2,23 @@ import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/visitor.dart';
 
 /// L10nVisitor
-class L10nVisitor extends GeneralizingAstVisitor<bool> {
+class L10nVisitor extends GeneralizingAstVisitor<void> {
   /// L10nVisitor
   L10nVisitor(this._value);
 
-  final String _value;
+  final Iterable<String> _value;
+
+  final _invocations = <String>{};
+
+  /// invocations
+  Set<String> get invocations => _invocations;
+
   @override
-  bool? visitSimpleIdentifier(final SimpleIdentifier node) {
-    if (node.name == _value) {
-      return true;
+  void visitSimpleIdentifier(final SimpleIdentifier node) {
+    if (_value.contains(node.name)) {
+      _invocations.add(node.name);
+      return;
     }
-    return false;
+    return;
   }
 }
