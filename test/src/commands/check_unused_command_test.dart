a4d1cdffd09901f43eeb28a81ba3b567548f438f
lsaudon <lsaudon@gmail.com>
2023-02-15 13:32:01 +0100

    test: remove useless code

--- test/src/commands/check_unused_command_test.dart
+++ test/src/commands/check_unused_command_test.dart
@@ -11,9 +11,7 @@ import '../mocks/mocks.dart';
 
 const l10nFileContent = '''
 arb-dir: lib/l10n/arb
-template-arb-file: app_en.arb
-output-localization-file: app_localizations.dart
-nullable-getter: false''';
+template-arb-file: app_en.arb''';
 
 const arbFileContentSimple = '''
 {
@@ -23,142 +21,17 @@ const arbFileContentSimple = '''
   "seeingTheWorldAgain": "Seeing the world again"
 }''';
 
-const appLocalizationsEnFileContent = '''
-import 'app_localizations.dart';
-
-class AppLocalizationsEn extends AppLocalizations {
-  AppLocalizationsEn([String locale = 'en']) : super(locale);
-
-  @override
-  String get helloMoon => 'Hello Moon';
-
-  @override
-  String get helloWorld => 'Hello World';
-
-  @override
-  String get seeingTheWorldAgain => 'Seeing the world again';
-}''';
-
-const appLocalizationsFileContent = r'''
-import 'dart:async';
-
-import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:intl/intl.dart' as intl;
-
-import 'app_localizations_en.dart';
-
-abstract class AppLocalizations {
-  AppLocalizations(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
-
-  final String localeName;
-
-  static AppLocalizations of(BuildContext context) {
-    return Localizations.of<AppLocalizations>(context, AppLocalizations)!;
-  }
-
-  static const LocalizationsDelegate<AppLocalizations> delegate = _AppLocalizationsDelegate();
-
-  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
-    delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-  ];
-
-  static const List<Locale> supportedLocales = <Locale>[
-    Locale('en')
-  ];
-
-  String get helloMoon;
-
-  String get helloWorld;
-
-  String get seeingTheWorldAgain;
-}
-
-class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
-  const _AppLocalizationsDelegate();
-
-  @override
-  Future<AppLocalizations> load(Locale locale) {
-    return SynchronousFuture<AppLocalizations>(lookupAppLocalizations(locale));
-  }
-
-  @override
-  bool isSupported(Locale locale) => <String>['en'].contains(locale.languageCode);
-
-  @override
-  bool shouldReload(_AppLocalizationsDelegate old) => false;
-}
-
-AppLocalizations lookupAppLocalizations(Locale locale) {
-  switch (locale.languageCode) {
-    case 'en': return AppLocalizationsEn();
-  }
-
-  throw FlutterError(
-    'AppLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
-    'an issue with the localizations generation tool. Please file an issue '
-    'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.'
-  );
-}
-''';
-
-const l10nDartFileContent = '''
-export 'package:flutter_gen/gen_l10n/app_localizations.dart';''';
-
 const mainFileContent = '''
-import 'package:example/l10n/l10n.dart';
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(final BuildContext context) => const MaterialApp(
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        home: _HomePage(),
-      );
-}
-
-class _HomePage extends StatelessWidget {
-  const _HomePage();
-
-  @override
-  Widget build(final BuildContext context) {
-    final l10n = AppLocalizations.of(context);
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(AppLocalizations.of(context).helloWorld),
-            Text(AppLocalizations.of(context).helloWorld),
-            Text(l10n.helloMoon),
-            Text(Stuff().seeingTheWorldAgain),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Stuff {
-  String get seeingTheWorldAgain => 'Seeing the world again';
+  AppLocalizations.of(context).helloWorld;
+  AppLocalizations.of(context).helloWorld;
+  l10n.helloMoon;
+  Stuff().seeingTheWorldAgain;
 }''';
 
 void main() {
   group('check-unused', () {
     test('When l10n.yaml is right', () async {
-      final logger = MockLogger();
       final mfs = MemoryFileSystem.test(
         style: Platform.isWindows
             ? FileSystemStyle.windows
@@ -166,12 +39,7 @@ void main() {
       );
 
       <String, String>{
-        '.dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart':
-            appLocalizationsEnFileContent,
-        '.dart_tool/flutter_gen/gen_l10n/app_localizations.dart':
-            appLocalizationsFileContent,
         'lib/l10n/arb/app_en.arb': arbFileContentSimple,
-        'lib/l10n/l10n.dart': l10nDartFileContent,
         'lib/main.dart': mainFileContent,
         'l10n.yaml': l10nFileContent,
       }.forEach(
@@ -180,12 +48,11 @@ void main() {
           ..writeAsStringSync(content),
       );
 
-      final commandRunner = L10nizationCliCommandRunner(
+      final logger = MockLogger();
+      final exitCode = await L10nizationCliCommandRunner(
         logger: logger,
         fileSystem: mfs,
-      );
-
-      final exitCode = await commandRunner.run(
+      ).run(
         [CheckUnusedCommand.commandName],
       );
 
@@ -195,9 +62,5 @@ void main() {
 
       expect(exitCode, ExitCode.success.code);
     });
-
-    // PropertyAccessImpl (AppLocalizations.of(context).helloWorld)
-    // MethodInvocationImpl (AppLocalizations.of(context))
-    // SimpleIdentifierImpl (helloWorld)
   });
 }
